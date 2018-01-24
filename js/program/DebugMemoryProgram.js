8ccdfc18a9488acd44987d39e0d51cd42790fe53
cphartman <cphartman@gmail.com>
2018-01-24 13:59:42 -0500

    Quickly jump to debug memory addresses  * Added JumpTo subscription listener to Debug Memory program.  * Added global .memory-link event listener to trigger JumpTo event.  * Added memory-links in the Debug State program

--- js/program/DebugMemoryProgram.js
+++ js/program/DebugMemoryProgram.js
@@ -27,6 +27,58 @@ var DebugMemoryProgram = function(emulation_core) {
 		this.scrollbar.callback = this.domEvents['scroll'].bind(this);
 
 		this.Refresh();
+
+		PubSub.subscribe("Debugger.Memory.JumpTo",function (msg, data) {
+			this.JumpTo(data);
+			this.Refresh();
+		}.bind(this));
+
+		this.SetupMemoryLinkEvent();
+	}
+
+	this.JumpTo = function(address) {
+		this.addressTop = (address & 0xFFF0) - 0x0080;
+		if( this.addressTop < 0 ) {
+			this.addressTop = 0;
+		}
+
+		this.scrollbar.Set(this.addressTop);
+		this.Refresh();
+		
+		Vue.nextTick(function(){
+			this.SelectText(address);	
+		}.bind(this));		
+	}
+
+	this.SelectText = function(address) {
+		var $address = document.querySelector(".memory-value[data-address='"+int2hex(address,4)+"']");
+		if( $address ) {
+			selection = window.getSelection();        
+	        range = document.createRange();
+	        range.selectNodeContents($address);
+	        selection.removeAllRanges();
+	        selection.addRange(range);
+	    }
+	}
+
+	this.SetupMemoryLinkEvent = function() {
+		document.body.addEventListener("click", function(e){
+			
+			var $iterator = e.target;
+			while( $iterator ) {
+				if( $iterator.classList.contains("memory-link") ) {
+					break;
+				}
+				$iterator = $iterator.parentElement;
+			}
+
+			if( $iterator ) {
+				var string = $iterator.innerText.toLowerCase().replace(/[^0-9a-f]/i,"");
+				var address = parseInt(string,16);
+				PubSub.publish("Debugger.Memory.JumpTo", address);
+			}
+
+		});
 	}
 
 	this.Refresh = function() {
@@ -43,12 +95,16 @@ var DebugMemoryProgram = function(emulation_core) {
 			this.view.memory_rows[i] = {
 				label: GameBoyCore.GetMemoryRegion(address),
 				address: int2hex(address,4),
-				values: []
+				columns: []
 			}
 
 			for( var m = 0; m < 16; m++ ) {
-				var value = DebugReadMemory(address+m);
-				this.view.memory_rows[i].values[m] = int2hex(value,2);
+				var column_address = address+m;
+				var value = DebugReadMemory(column_address);
+				this.view.memory_rows[i].columns[m] = {
+					address: int2hex(column_address,4),
+					value: int2hex(value,2),
+				};
 			}
 		}
 	}
