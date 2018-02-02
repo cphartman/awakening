a82f19738683a53f664a464dcbd06281bf16b87c
cphartman <cphartman@gmail.com>
2018-02-02 01:03:42 -0500

    UI Updates  * Added fonts  * Templated window component and lcd program  * Basic styling for windows  * Titles for windows

--- components/window.js
+++ components/window.js
@@ -1,25 +1,53 @@
 var Window = function() {
 	this.root = document.querySelector("body");
 
+	this.$window = false;
+	this.programWindow = false;
+
 	this.top = 0;
 	this.left = 0;
 	this.width = 100;
 	this.height = 100;
 	this.$el = false;
+	this.title = "";
+	this.template = `
+		<div class='window-padding'>
+			<div class='window-titleBar'>
+				<div class='window-titleName'></div>
+				<div class='window-titleTools'></div>
+			</div>
+			<div class='program-window'></div>
+		</div>
+	`;
 
 	this.Init = function(template) {
-		this.$el = document.createElement("div");
-		this.$el.classList.add("window");
+		this.$window = document.createElement("div");
+		this.$window.classList.add("window");
+		this.$window.innerHTML = this.template;
+		
+		this.$el = this.$window.querySelector(".program-window");
 		this.$el.innerHTML = template;
 
-		this.root.appendChild(this.$el);
+		this.$title = this.$window.querySelector(".window-titleName");
+		this.$title.innerHTML = this.title;
+		
+		this.root.appendChild(this.$window);
 		this.Refresh();
 	}
 
 	this.Refresh = function() {
-		this.$el.style['top'] = this.top+"px";
-		this.$el.style['left'] = this.left+"px";
-		this.$el.style['width'] = this.width+"px";
-		this.$el.style['height'] = this.height+"px";
+		this.$window.style['top'] = this.top+"px";
+		this.$window.style['left'] = this.left+"px";
+		this.$window.style['width'] = this.width+"px";
+		this.$window.style['height'] = this.height+"px";
+
+		this.$title.innerHTML = this.title;
+	}
+
+	this.ProgramSize = function() {
+		return {
+			width: this.$programWindow.offsetWidth,
+			height: this.$programWindow.offsetHeight
+		};
 	}
 }
--- programs/DebugLCDProgram.js
+++ programs/DebugLCDProgram.js
@@ -1,15 +1,22 @@
 var DebugLCDProgram = function(emulation_core) {
 	this.emulation_core = emulation_core;
 	this.$window = false;
+	this.template = `
+	<div class='debug-lcd-window'></div>
+	`;
 
 	this.Init = function() {
-		var $window = this.window.$el;
-		this.$window = $window;
+
+		this.$window = this.window.$el;
 		this.$window.innerHTML = "<div class='debug-lcd-window'></div>";
 		this.$window.querySelector(".debug-lcd-window").appendChild(this.emulation_core.canvas);
 	}
 
 	this.Refresh = function() {
+		if( !this.$window.querySelector("canvas") ) {
+			this.$window.innerHTML = "<div class='debug-lcd-window'></div>";
+			this.$window.querySelector(".debug-lcd-window").appendChild(this.emulation_core.canvas);
+		}
 	}
 }
 
